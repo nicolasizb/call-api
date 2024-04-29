265a8543de027f9861eb3b94d4dc84637d32a5d7
nicolasizb <nicolasizquierdocontacto@gmail.com>
2024-04-29 17:30:04 -0500

    Tried

--- index.js
+++ index.js
@@ -1,70 +1,33 @@
-require('dotenv').config()
-
-const express = require('express')
-
-const accountSid = 'AC543fc2f1fd0ee20ea8908462645ea542';
-const authToken = '969eb9dc66ef04de0ddfdcb1b9628cf8';
-
-// twilio.calls
-//       .create({
-//          url: 'https://handler.twilio.com/twiml/EH3ff25c2abb305b0788c745c1f123cce7',
-//          to: '+573102950378',
-//          from: '+13343100649'
-//        })
-//       .then(call => console.log(call.sid));
-
-
-const bodyParser = require('body-parser');
+const express = require('express');
 const twilio = require('twilio');
+require('dotenv').config()  
 
 const app = express();
-const port = process.env.PORT;
 
-// Configurar Twilio
-const client = twilio(accountSid, authToken);
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioClient = twilio(accountSid, authToken);
 
-// Configurar bodyParser para procesar JSON
-app.use(bodyParser.json());
+app.get('/makeCall', async (req, res) => {
+  const phoneNumber = req.query.phoneNumber;
+  const shippingAddress = req.query.shippingAddress;
+  const CustomerName = req.query.CustomerName;
 
-// Ruta para la solicitud de Zapier
-app.post('/twilio-api-request', async (req, res) => {
-  // Extraer datos de la solicitud
-  const method = req.body.method;
-  const url = req.body.url;
-  const headers = req.body.headers;
-  const body = req.body.body;
-
-  // Crear opciones de solicitud
-  const options = { 
-    method,
-    url,
-    headers,
-  };
-
-  // Si hay un cuerpo, agregarlo a las opciones
-  if (body) {
-    options.body = body;
-  }
-
-  // Realizar la solicitud a la API de Twilio
   try {
-    const response = await client.request(options);
-    res.json({
-      success: true,
-      statusCode: response.statusCode,
-      headers: response.headers,
-      body: response.body,
+    await twilioClient.calls.create({
+      to: phoneNumber,
+      url: 'https://handler.twilio.com/twiml/EH3ff25c2abb305b0788c745c1f123cce7',
+    });
+
+    res.send({
+      message: `Llamada realizada al número ${phoneNumber}. Dirección de envío: ${shippingAddress}`,
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({
-      success: false,
-      error: error.message,
-    });
+    res.status(500).send({ error: error.message });
   }
 });
 
-// Iniciar el servidor
-app.listen(port, () => {
-  console.log(`Servidor escuchando en el puerto ${port}`);
+app.listen(3000, () => {
+  console.log('Servidor API Twilio escuchando en el puerto 3000');
 });
