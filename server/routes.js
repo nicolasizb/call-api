90e4b764cd2d6e1a9970e2b7c5bc627ce6bc52ba
nicolasizb <nicolasizquierdocontacto@gmail.com>
2024-05-02 16:43:36 -0500

    Add all queries

--- server/routes.js
+++ server/routes.js
@@ -11,57 +11,59 @@ router.get('/', (req, res) => {
 })
 
 router.post('/call', async (req, res) => {
-    const supportNumber = process.env.SUPPORT_NUMBER
-    const clientNumber = req.query.clientNumber
-    const addressOne = req.query.addressOne
-    const city = req.query.city
-    const store = req.query.store
-    const customerName = req.query.customerName || ""
+    
+    if (!req.body.clientNumber || !req.body.addressOne || !req.body.city || !req.body.city || !req.body.store || !req.body.customerName) {
+        res.status(400).json({ error: "Invalid data" })
+    } else {
+        const supportNumber = process.env.SUPPORT_NUMBER
+        
+        const { clientNumber, addressOne, city, store, customerName } = req.body
+        
+        try {
+            const twiml = new VoiceResponse()
+            twiml.say(
+                {
+                    language: 'es',
+                    voice: 'Polly.Mia-Neural'
+                },
+                `Hola ${customerName}, lo llamamos desde la tienda ${store} para confirmar la dirección de envío de su pedido. ¿Su dirección es ${addressOne} en ${city}?`
+            )
 
-    try {
-        const twiml = new VoiceResponse()
-        twiml.say(
-            {
-                language: 'es',
-                voice: 'Polly.Mia-Neural'
-            },
-            `Hola ${customerName}, lo llamamos desde la tienda ${store} para confirmar la dirección de envío de su pedido. ¿Su dirección es ${addressOne} en ${city}?`
-        )
+            const gather = twiml.gather({
+                numDigits: 1,
+                action: 'https://flame-harrier-8893.twil.io/validation',
+                method: 'POST'
+            })
 
-        const gather = twiml.gather({
-            numDigits: 1,
-            action: 'https://flame-harrier-8893.twil.io/validation',
-            method: 'POST'
-        })
+            gather.say(
+                {
+                    language: 'es',
+                    voice: 'Polly.Mia-Neural'
+                },
+                'Por favor marque el número 1, para confirmar que está correcta la dirección.'
+            )
 
-        gather.say(
-            {
-                language: 'es',
-                voice: 'Polly.Mia-Neural'
-            },
-            'Por favor marque el número 1, para confirmar que está correcta la dirección.'
-        )
+            gather.say(
+                {
+                    language: 'es',
+                    voice: 'Polly.Mia-Neural'
+                },
+                'O marque el número 2, para cambiar la dirección de envío de su pedido.'
+            )
 
-        gather.say(
-            {
-                language: 'es',
-                voice: 'Polly.Mia-Neural'
-            },
-            'O marque el número 2, para cambiar la dirección de envío de su pedido.'
-        )
+            const call = await twilio.calls.create({
+                twiml: twiml.toString(),
+                to: clientNumber,
+                from: supportNumber
+            });
 
-        const call = await twilio.calls.create({
-            twiml: twiml.toString(),
-            to: clientNumber,
-            from: supportNumber
-        });
+            console.log(call.sid)
 
-        console.log(call.sid)
-
-        res.type('text/xml').send(twiml.toString())
-    } catch (error) {
-        console.error(error)
-        res.status(500).send('Error al realizar la llamada')
+            res.type('text/xml').send(twiml.toString())
+        } catch (error) {
+            console.error(error)
+            res.status(500).send('Error al realizar la llamada')
+        }
     }
 })
 
