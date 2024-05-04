79d26a4d5f251518736091cc049fd8f79d5b8c5e
nicolasizb <nicolasizquierdocontacto@gmail.com>
2024-05-03 19:18:12 -0500

    just good

--- server/routes.js
+++ server/routes.js
@@ -11,87 +11,82 @@ router.get('/', (req, res) => {
 })
 
 router.post('/call', async (req, res) => {
+    
     if (!req.body.clientNumber || !req.body.addressOne || !req.body.addressDetails || !req.body.city || !req.body.city || !req.body.store || !req.body.firstName || !req.body.lastName ) {
         res.status(400).json({ error: "Invalid data" })
-        return;
-    }
-    
-    const supportNumber = process.env.SUPPORT_NUMBER
-    const { clientNumber, addressOne, addressDetails, city, store, firstName, lastName } = req.body
-    
-    try {
-        const twiml = new VoiceResponse()
-        twiml.say(
-            {
-                language: 'es',
-                voice: 'Polly.Mia-Neural'
-            },
-            `Hola ${firstName} ${lastName}, lo llamamos desde la tienda ${store} para confirmar la dirección de envío de su pedido. ¿Su dirección es ${addressOne} ${addressDetails} en ${city}?`
-        )
+    } else {
+        const supportNumber = process.env.SUPPORT_NUMBER
+        
+        const { clientNumber, addressOne, addressDetails, city, store, firstName, lastName } = req.body
+        
+        try {
+            const twiml = new VoiceResponse()
+            twiml.say(
+                {
+                    language: 'es',
+                    voice: 'Polly.Mia-Neural'
+                },
+                `Hola ${firstName} ${lastName}, lo llamamos desde la tienda ${store} para confirmar la dirección de envío de su pedido. ¿Su dirección es ${addressOne} ${addressDetails} en ${city}?`
+            )
 
-        const gather = twiml.gather({
-            numDigits: 1,
-            action: 'https://call-api-phi.vercel.app/validation',
-            method: 'POST'
-        })
-
-        gather.say(
-            {
-                language: 'es',
-                voice: 'Polly.Mia-Neural'
-            },
-            'Por favor marque el número 1, para confirmar que está correcta la dirección. O marque el número 2, para cambiar la dirección de envío de su pedido.'
-        )
-
-        const call = await twilio.calls.create({
-            twiml: twiml.toString(),
-            to: clientNumber,
-            from: supportNumber
-        })
-
-        console.log(call.sid)
-
-        // Crear una promesa que se resolverá cuando se presione un dígito
-        const digitPressedPromise = new Promise(resolve => {
-            router.post('https://call-api-phi.vercel.app/validation', (req, res) => {
-                const digitPressed = req.body.Digits
-                const twiml = new VoiceResponse()
-
-                switch (digitPressed) { 
-                    case '1':
-                        twiml.say({
-                            language: 'es',
-                            voice: 'Polly.Mia-Neural'
-                        }, 'Usted acaba de confirmar que la dirección mencionada es correcta, nos pondremos en contacto con usted por WhatsApp para confirmar fecha de envío.')
-                        break;
-                    case '2':
-                        twiml.say({
-                            language: 'es',
-                            voice: 'Polly.Mia-Neural'
-                        },'Usted acaba de confirmar que su dirección es incorrecta, procederemos a editar su dirección')
-                        break;
-                    default:
-                        twiml.say({
-                            language: 'es',
-                            voice: 'Polly.Mia-Neural'
-                        }, 'Opción no válida. Por favor, intenta de nuevo.')
-                        break;
-                }
-
-                res.type('text/xml').send(twiml.toString())
-
-                resolve(digitPressed)
+            const gather = twiml.gather({
+                numDigits: 1,
+                action: 'https://call-api-phi.vercel.app/validation',
+                method: 'POST'
             })
-        })
 
-        // Esperar a que se resuelva la promesa
-        const digitPressed = await digitPressedPromise
+            gather.say(
+                {
+                    language: 'es',
+                    voice: 'Polly.Mia-Neural'
+                },
+                'Por favor marque el número 1, para confirmar que está correcta la dirección. O marque el número 2, para cambiar la dirección de envío de su pedido.'
+            )
 
-        res.status(200).json({ digitStatus: digitPressed })
-    } catch (error) {
-        console.error(error)
-        res.status(500).send('Error al realizar la llamada')
+            const call = await twilio.calls.create({
+                twiml: twiml.toString(),
+                to: clientNumber,
+                from: supportNumber
+            })
+
+            console.log(call.sid)
+
+            res.type('text/xml').send(twiml.toString())
+            
+        } catch (error) {
+            console.error(error)
+            res.status(500).send('Error al realizar la llamada')
+        }
     }
+})
+
+router.post('/validation', (req, res) => {
+    const twiml = new VoiceResponse()
+    
+    const digitPressed = req.body.Digits;
+
+    switch (digitPressed) { 
+        case '1':
+            twiml.say({
+                language: 'es',
+                voice: 'Polly.Mia-Neural'
+            }, 'Usted acaba de confirmar que la dirección mencionada es correcta, nos pondremos en contacto con usted por WhatsApp para confirmar fecha de envío.')
+            break;
+        case '2':
+            twiml.say({
+                language: 'es',
+                voice: 'Polly.Mia-Neural'
+            },'Usted acaba de confirmar que su dirección es incorrecta, procederemos a editar su dirección')
+            break;
+        default:
+            twiml.say({
+                language: 'es',
+                voice: 'Polly.Mia-Neural'
+            }, 'Opción no válida. Por favor, intenta de nuevo.')
+            break;
+    }
+    
+    res.type('text/xml').send(twiml.toString())
 })
 
 module.exports = router
