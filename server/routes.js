2917d44317dca7ea16575efdd02d2b2686799af2
nicolasizquierdo <nicolasizquierdocontacto@gmail.com>
2024-05-18 16:21:19 -0500

    Add new big changes 2

--- server/routes.js
+++ server/routes.js
@@ -69,7 +69,7 @@ router.post('/call', async (req, res) => {
                 numDigits: 1,
                 action: 'https://call-api-phi.vercel.app/validation',
                 method: 'POST',
-                timeout: 4
+                timeout: 10
             });
         
             repeatGather.say({
@@ -128,7 +128,7 @@ router.post('/validation', async (req, res) => {
                     numDigits: 1,
                     action: 'https://call-api-phi.vercel.app/change-address',
                     method: 'POST',
-                    timeout: 4
+                    timeout: 10
                 })
 
                 gather.say({
@@ -136,6 +136,28 @@ router.post('/validation', async (req, res) => {
                     voice: 'Polly.Mia-Neural'
                 }, `Marque el número 1, si está correcta. O marque el número 2 para cambiar dirección de envío.`)
 
+                for (let i = 0; i<= 2; i++) {
+                    const repeatGather = twiml.gather({
+                        numDigits: 1,
+                        action: 'https://call-api-phi.vercel.app/validation',
+                        method: 'POST',
+                        timeout: 10
+                    });
+                
+                    repeatGather.say({
+                        language: 'es-MX',
+                        voice: 'Polly.Mia-Neural'
+                    }, 'Marque el número 1, si está correcta. O marque el número 2 para cambiar dirección de envío.')
+        
+                    if(i === 2) {
+                        changeData(undefined, undefined, undefined, 'Change', undefined)        
+                    }
+                }
+
+                twiml.say({
+                    language: 'es-MX',
+                    voice: 'Polly.Mia-Neural'
+                }, 'Nos pondremos en contacto con usted por correo electrónico para confirmar su dirección.')
                 break;
             default:
                 for (let i = 0; i<= 2; i++) {
@@ -191,13 +213,36 @@ router.post('/change-address', async (req, res) => {
                     numDigits: 1,
                     action: 'https://call-api-phi.vercel.app/filter',
                     method: 'POST',
-                    timeout: 4
+                    timeout: 10
                 })
             
                 gather.say({
                     language: 'es-MX',
                     voice: 'Polly.Mia-Neural'
                 }, `Marque 1 para autorizar que lo contactemos al correo electrónico para cambiar la dirección. O marque 2 para confirmar que la dirección nombrada es correcta.`)
+
+                for (let i = 0; i<= 2; i++) {
+                    const repeatGather = twiml.gather({
+                        numDigits: 1,
+                        action: 'https://call-api-phi.vercel.app/validation',
+                        method: 'POST',
+                        timeout: 10
+                    });
+                
+                    repeatGather.say({
+                        language: 'es-MX',
+                        voice: 'Polly.Mia-Neural'
+                    }, 'Marque 1 para autorizar que lo contactemos al correo electrónico para cambiar la dirección. O marque 2 para confirmar que la dirección nombrada es correcta.')
+        
+                    if(i === 2) {
+                        changeData(undefined, undefined, undefined, 'Change', undefined)        
+                    }
+                }
+
+                twiml.say({
+                    language: 'es-MX',
+                    voice: 'Polly.Mia-Neural'
+                }, 'Nos pondremos en contacto con usted por correo electrónico para confirmar su dirección.')
                 break;
             default:
                 for (let i = 0; i<= 2; i++) {
@@ -265,7 +310,7 @@ router.post('/send-email', async(req, res) => {
                         numDigits: 1,
                         action: 'https://call-api-phi.vercel.app/send-email',
                         method: 'POST',
-                        timeout: 4
+                        timeout: 10
                     })
                 
                     gather.say({
