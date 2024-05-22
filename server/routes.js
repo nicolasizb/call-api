707ee559e39f848ddb8d7a5be02ca53d3eca684d
nicolasizquierdo <nicolasizquierdocontacto@gmail.com>
2024-05-22 13:35:57 -0500

    Update with new settings 3

--- server/routes.js
+++ server/routes.js
@@ -138,34 +138,34 @@ router.post('/validation', async (req, res) => {
                     voice: 'Polly.Mia-Neural'
                 }, `Marque el número 1, si está correcta. O marque el número 2 para cambiar dirección de envío.`)
 
-                // for (let i = 0; i<= 2; i++) {
-                //     twiml.say({
-                //         language: 'es-MX',
-                //         voice: 'Polly.Mia-Neural'
-                //     }, `¿Su dirección es ${userData.address}?`)
+                for (let i = 0; i<= 2; i++) {
+                    twiml.say({
+                        language: 'es-MX',
+                        voice: 'Polly.Mia-Neural'
+                    }, `¿Su dirección es ${userData.address}?`)
 
-                //     const repeatGather = twiml.gather({
-                //         numDigits: 1,
-                //         action: 'https://call-api-phi.vercel.app/change-address',
-                //         method: 'POST',
-                //         timeout: 10
-                //     });
+                    const repeatGather = twiml.gather({
+                        numDigits: 1,
+                        action: 'https://call-api-phi.vercel.app/change-address',
+                        method: 'POST',
+                        timeout: 10
+                    });
                 
-                //     repeatGather.say({
-                //         language: 'es-MX',
-                //         voice: 'Polly.Mia-Neural'
-                //     }, 'Marque el número 1, si está correcta. O marque el número 2 para cambiar dirección de envío.')
+                    repeatGather.say({
+                        language: 'es-MX',
+                        voice: 'Polly.Mia-Neural'
+                    }, 'Marque el número 1, si está correcta. O marque el número 2 para cambiar dirección de envío.')
         
-                //     if(i === 2) {
-                //         changeData(undefined, undefined, undefined, 'Change', undefined)        
-                //     }
-                // }
+                    if(i === 2) {
+                        changeData(undefined, undefined, undefined, 'Change', undefined)        
+                    }
+                }
 
-                // twiml.say({
-                //     language: 'es-MX',
-                //     voice: 'Polly.Mia-Neural'
-                // }, 'Nos pondremos en contacto con usted por whatsapp para confirmar su dirección.')
-                // break;
+                twiml.say({
+                    language: 'es-MX',
+                    voice: 'Polly.Mia-Neural'
+                }, 'Nos pondremos en contacto con usted por whatsapp para confirmar su dirección.')
+                break;
             default:
                 for (let i = 0; i<= 2; i++) {
                     const gather = twiml.gather({
@@ -225,30 +225,30 @@ router.post('/change-address', async (req, res) => {
                 gather.say({
                     language: 'es-MX',
                     voice: 'Polly.Mia-Neural'
-                }, `Marque 1 para autorizar que lo contactemos al whatsapp para cambiar la dirección. O marque 2 para confirmar que la dirección nombrada es correcta.`)
+                }, `Marque 1 para autorizar que lo contactemos al whatsapp para cambiar la dirección. O marque 2 para confirmar que la dirección ${userData.address} es correcta.`)
 
-                // for (let i = 0; i<= 2; i++) {
-                //     const repeatGather = twiml.gather({
-                //         numDigits: 1,
-                //         action: 'https://call-api-phi.vercel.app/filter',
-                //         method: 'POST',
-                //         timeout: 10
-                //     });
+                for (let i = 0; i<= 2; i++) {
+                    const repeatGather = twiml.gather({
+                        numDigits: 1,
+                        action: 'https://call-api-phi.vercel.app/filter',
+                        method: 'POST',
+                        timeout: 10
+                    });
                 
-                //     repeatGather.say({
-                //         language: 'es-MX',
-                //         voice: 'Polly.Mia-Neural'
-                //     }, 'Marque 1 para autorizar que lo contactemos al whatsapp para cambiar la dirección. O marque 2 para confirmar que la dirección nombrada es correcta.')
+                    repeatGather.say({
+                        language: 'es-MX',
+                        voice: 'Polly.Mia-Neural'
+                    }, 'Marque 1 para autorizar que lo contactemos al whatsapp para cambiar la dirección. O marque 2 para confirmar que la dirección nombrada es correcta.')
         
-                //     if(i === 2) {
-                //         changeData(undefined, undefined, undefined, 'Change', undefined)        
-                //     }
-                // }
+                    if(i === 2) {
+                        changeData(undefined, undefined, undefined, 'Change', undefined)        
+                    }
+                }
 
-                // twiml.say({
-                //     language: 'es-MX',
-                //     voice: 'Polly.Mia-Neural'
-                // }, 'Nos pondremos en contacto con usted por whatsapp para confirmar su dirección.')
+                twiml.say({
+                    language: 'es-MX',
+                    voice: 'Polly.Mia-Neural'
+                }, 'Nos pondremos en contacto con usted por whatsapp para confirmar su dirección.')
                 break;
             default:
                 for (let i = 0; i<= 2; i++) {
