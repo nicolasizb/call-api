7de6cf72671ea8ac6c6762e0345823111152448b
nicolasizquierdo <nicolasizquierdocontacto@gmail.com>
2024-05-27 13:10:21 -0500

    try update change function 4

--- server/routes.js
+++ server/routes.js
@@ -115,7 +115,7 @@ router.post('/validation', async (req, res) => {
 
         switch (digitPressed) { 
             case '1':
-                changeData(undefined, undefined, undefined, undefined, undefined, 'Confirmado', undefined)      
+                changeData({digit:'Confirmado'})      
 
                 if(userData.store == 'Velez') {
                     await axios.post('https://hooks.zapier.com/hooks/catch/18861658/3vks138/', userData)
