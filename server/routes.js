2fcf8e109db31bbcad5c41124d7987e125d94b36
nicolasizquierdo <nicolasizquierdocontacto@gmail.com>
2024-05-30 11:17:28 -0500

    reset to good reading

--- server/routes.js
+++ server/routes.js
@@ -14,17 +14,17 @@ let userData = {
     address: '',
     city: '',
     digit: '',  
-    dataCall: ''
-}
+    callSID: '',
+};
 
-function changeData(userID, store, number, address, city, digit, dataCall) {
+function changeData(userID, store, number, address, city, digit, callSID) {
     if (userID !== undefined) userData.userID = userID
     if (store !== undefined) userData.store = store
     if (number !== undefined) userData.number = number
     if (address !== undefined) userData.address = address
     if (city !== undefined) userData.city = city
     if (digit !== undefined) userData.digit = digit
-    if (dataCall !== undefined) userData.dataCall = dataCall
+    if (callSID !== undefined) userData.callSID = callSID
 }
 
 function processAddress(address) {
@@ -41,12 +41,14 @@ router.post('/call', async (req, res) => {
         }
 
         const setAddress = processAddress(`${addressOne}, ${addressDetails || ''}`)
-                
+
+        twiml.pause({ length: 2 })
+        
         twiml.say({ 
             language: 'es-MX',
             voice: 'Polly.Mia-Neural',
             rate: '82%'
-        }, `Hola ${firstName} ${lastName}! Le llamamos de la tienda ${store} para confirmar la dirección de envío de su pedido. ¿Es correcta la dirección: ${setAddress}, en ${city}?`)
+        },`Hola ${firstName} ${lastName}! Le llamamos de la tienda ${store} para confirmar la dirección de envío de su pedido. ¿Es correcta la dirección: ${setAddress}, en ${city}`)
         
         const gather = twiml.gather({
             numDigits: 1,
@@ -119,11 +121,9 @@ router.post('/validation', async (req, res) => {
                     await axios.post('https://hooks.zapier.com/hooks/catch/18861658/3vks138/', userData)
                 } else if (userData.store == 'Will') {
                     await axios.post('https://hooks.zapier.com/hooks/catch/18861658/3vq7qsy/', userData)
-                } else if (userData.store == 'Test') {
-                    await axios.post('https://hooks.zapier.com/hooks/catch/18861658/2y49aol/', userData)
                 } else {
+                    // another webhook
                     console.error('Not found store')
-                    req.status('Not found store')
                 }
 
                 twiml.say({
@@ -131,7 +131,6 @@ router.post('/validation', async (req, res) => {
                     voice: 'Polly.Mia-Neural',
                     rate: '82%'
                 }, 'Usted confirmó que la dirección mencionada es correcta, gracias por su respuesta. ¡Hasta luego!')
-
                 break;
             case '2':
                 twiml.say({
@@ -227,15 +226,12 @@ router.post('/change-address', async (req, res) => {
             case '1' :
                 changeData(undefined, undefined, undefined, undefined, undefined, 'Confirmado', undefined)       
                 
-                if(userData.store == 'Velez') {
+                if(userData.store === 'Velez') {
                     await axios.post('https://hooks.zapier.com/hooks/catch/18861658/3vks138/', userData)
-                } else if (userData.store == 'Will') {
+                } else if (userData.store === 'Will') {
                     await axios.post('https://hooks.zapier.com/hooks/catch/18861658/3vq7qsy/', userData)
-                } else if (userData.store == 'Test') {
-                    await axios.post('https://hooks.zapier.com/hooks/catch/18861658/2y49aol/', userData)
                 } else {
                     console.error('Not found store')
-                    req.status('Not found store')
                 }
         
                 twiml.say({
@@ -327,15 +323,12 @@ router.post('/send-message', async(req, res) => {
             case '1':
                 changeData(undefined, undefined, undefined, undefined, undefined, 'Cambiar', undefined)       
                 
-                if(userData.store == 'Velez') {
+                if(userData.store === 'Velez') {
                     await axios.post('https://hooks.zapier.com/hooks/catch/18861658/3vks138/', userData)
-                } else if (userData.store == 'Will') {
+                } else if (userData.store === 'Will') {
                     await axios.post('https://hooks.zapier.com/hooks/catch/18861658/3vq7qsy/', userData)
-                } else if (userData.store == 'Test') {
-                    await axios.post('https://hooks.zapier.com/hooks/catch/18861658/2y49aol/', userData)
                 } else {
                     console.error('Not found store')
-                    req.status('Not found store')
                 }
 
                 twiml.say({
@@ -433,15 +426,12 @@ router.post('/finish', async (req, res) => {
             case '1':
                 changeData(undefined, undefined, undefined, undefined, undefined, 'Confirmado', undefined)       
                 
-                if(userData.store == 'Velez') {
+                if(userData.store === 'Velez') {
                     await axios.post('https://hooks.zapier.com/hooks/catch/18861658/3vks138/', userData)
-                } else if (userData.store == 'Will') {
+                } else if (userData.store === 'Will') {
                     await axios.post('https://hooks.zapier.com/hooks/catch/18861658/3vq7qsy/', userData)
-                } else if (userData.store == 'Test') {
-                    await axios.post('https://hooks.zapier.com/hooks/catch/18861658/2y49aol/', userData)
                 } else {
                     console.error('Not found store')
-                    req.status('Not found store')
                 }
         
                 twiml.say({
